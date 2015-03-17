07895aec7ffb9210327f3666a8d8e1a286bd7048
odeleongt <odeleongt@gmail.com>
2015-03-17 15:16:08 -0600

    Add template for auto complete text input / output

--- app/ui.R
+++ app/ui.R
@@ -1,7 +1,17 @@
 
 shinyUI(
   bootstrapPage(
-    numericInput('n', 'Number of obs', 100),
-    plotOutput('plot')
+    includeCSS("./includes/awesomplete/awesomplete.css"),
+    includeScript("./includes/awesomplete/awesomplete.js"),
+    div(class="form-group shiny-input-container",
+        tags$label("Autocomplete"),
+        tags$input(id="text", type="text", class="awesomplete form-control",
+                   style="width:25em;",
+                   value="", autofocus="true", 'data-multiple'="true",
+                   'data-minchars'="1" , 'data-autofirst'="true",
+                   'data-list'= paste(scan(file = "./data/common-english-words",
+                                           what = "character"), collapse = " "))
+        ),
+    textOutput('text')
   )
 )
