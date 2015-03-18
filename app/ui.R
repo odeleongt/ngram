507b4b164bb36fde37f0aff6cd2bb2c21c8074b3
odeleongt <odeleongt@gmail.com>
2015-03-18 12:42:47 -0600

    Improve app layout

--- app/ui.R
+++ app/ui.R
@@ -3,15 +3,18 @@ shinyUI(
   bootstrapPage(
     includeCSS("./includes/awesomplete/awesomplete.css"),
     includeScript("./includes/awesomplete/awesomplete.js"),
-    div(class="form-group shiny-input-container",
-        tags$label("Autocomplete"),
-        tags$input(id="text", type="text", class="awesomplete form-control",
-                   style="width:25em;",
-                   value="", autofocus="true", 'data-multiple'="true",
-                   'data-minchars'="1" , 'data-autofirst'="true",
-                   'data-list'= paste(scan(file = "./data/common-english-words",
-                                           what = "character"), collapse = " "))
-        ),
-    textOutput('text')
+    fluidRow(
+      column(
+        width = 6,
+        div(class="form-group shiny-input-container",
+            tags$label("Autocomplete"),
+            tags$input(id="text", type="text", class="awesomplete form-control",
+                       style="width:25em;",
+                       value="", autofocus="true", 'data-multiple'="true",
+                       'data-minchars'="1" , 'data-autofirst'="true",
+                       'data-list'= paste(scan(file = "./data/common-english-words",
+                                               what = "character"), collapse = " "))
+        )),
+      column(width = 6, textOutput('text')))
   )
 )
