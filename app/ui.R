79b91119b61e05f549556f7acd1c88ca6268e19c
odeleongt [nb] <odeleongt@gmail.com>
2015-03-18 16:22:02 -0600

    Adjust style

--- app/ui.R
+++ app/ui.R
@@ -11,21 +11,23 @@ shinyUI(
              class="pull-left", "n-gram predictor"),
         div(class="pull-right", style="width:50px",
             tags$object(data="example.svg", type="image/svg+xml",
-                        width="50", height="50",
                         img(src="./img/repo_logo.svg", alt="n-gram",
                             width="50px", height="50px")))),
     fluidRow(
       column(
-        width = 6,
+        width = 4,
         div(class="form-group shiny-input-container",
-            tags$label("Autocomplete"),
-            tags$input(id="text", type="text", class="awesomplete form-control",
-                       style="width:25em;",
+            div(tags$label("Autocomplete")),
+            tags$input(id="text", type="text",
+                       class="awesomplete form-control",
+                       style="width:17em;",
                        value="", autofocus="true", 'data-multiple'="true",
                        'data-minchars'="1" , 'data-autofirst'="true",
                        'data-list'= paste(scan(file = "./data/common-english-words",
                                                what = "character"), collapse = " "))
         )),
-      column(width = 6, textOutput('text')))
+      column(
+        width = 4,
+        textOutput('text')))
   )
 )
