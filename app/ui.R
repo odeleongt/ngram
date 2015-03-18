aaa0445b18f695374cc3e17fc716c38e6b247445
odeleongt [nb] <odeleongt@gmail.com>
2015-03-18 15:54:57 -0600

    Configure own branding

--- app/ui.R
+++ app/ui.R
@@ -3,6 +3,17 @@ shinyUI(
   bootstrapPage(
     includeCSS("./includes/awesomplete/awesomplete.css"),
     includeScript("./includes/awesomplete/awesomplete.js"),
+    includeScript("./includes/remove-branding.js"),
+    div(style="width:100%;height:50px;background:#216699;",
+        span(style=paste0("font-family:\"Open Sans\",\"Helvetica Neue\",",
+                          "Arial,sans-serif;font-size:24px;color:#FFF;",
+                          "display:inline-block;line-height:50px;"),
+             class="pull-left", "n-gram predictor"),
+        div(class="pull-right", style="width:50px",
+            tags$object(data="example.svg", type="image/svg+xml",
+                        width="50", height="50",
+                        img(src="./img/repo_logo.svg", alt="n-gram",
+                            width="50px", height="50px")))),
     fluidRow(
       column(
         width = 6,
