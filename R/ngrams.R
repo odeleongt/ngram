3b0f746970db749c2801c91d58b8056d77695556
odeleongt [nb] <odeleongt@gmail.com>
2015-03-21 16:13:05 -0600

    More general function to prepare n-grams

---
+++ R/ngrams.R
@@ -0,0 +1,67 @@
+#' Build n-grams from tokens
+#' 
+#' n-grams will usually be sets of n tokens which are adjacent to each other in
+#' a corpus. The adjacency will be limited to some smaller set or semantic unit
+#' (\emph(e.g.) a sentence) and tokens from different sets should not be
+#' considered adjacent. An efficient way to store sets of tokens in R is a
+#' \code{list} (\emph{i.e.} the corpus) composed of \code{character} vectors
+#' (\emph{i.e.} the sets) containing the tokens as the vector elements.
+#' 
+#' n-grams can be obtained by binding the set of tokens to an equivalent but
+#' lagged set, and discarding the incomplete n-element groups. Subsetting each
+#' set to get variable-length n-grams would be very inefficient so a vector
+#' containing all the tokens is used instead, padding each set with \code{NA}s
+#' first to avoid a set into the next one.
+#' 
+#' @seealso \code{\link{assign}}
+#' @param tokens An object which can be coerced to \code{character} and contains
+#'   vectors of adjacent tokens (\emph{e.g.} a list of tokenized sentences).
+#' @param maxn The maximum number of grams to prepare from the tokens.
+#' @param tag A lenght one character to append to the name of the generated
+#'   data.tables.
+#' @param where Environment in which to assign each generated n-gram data.table.
+#'   By default assigns to the global environment, see \code{assign} for options.
+#' @export
+#' @examples
+#' #
+#' corpus <- c("This is a test sentence", "This is another test")
+#' tokens <- strsplit(corpus, split = " ")
+#' ngrams(tokens)
+#' @importFrom data.table data.table setnames
+#' @importFrom dplyr lag.default
+ngrams <- function(tokens, maxn = 3L, tag = "gram", where = search()[1]){
+  # Set of n-grams to prepare
+  ns <- paste0("n", seq(0, maxn-1))
+  
+  # Get individual tokens into a data.table as the stems
+  # NAs are used to align the unigram column to efficiently bind next n-1 tokens
+  tokens <- data.table(stem = unlist(lapply(tokens, c, rep(NA, maxn-1))))
+  setnames(tokens, "stem", ns[1])
+  
+  # Bind lagged tokens to stem.
+  # Each iteration binds the previous word to the last n words.
+  for(previous in seq(2, length(ns))){
+    tokens <- tokens[, ns[previous]:= lag(get(ns[previous-1]), n = 1)]
+  }
+  
+  # Remove NA stems used for alignment
+  tokens <- tokens[!is.na(get(ns[1]))]
+  
+  # Fake count for [individual] tokens
+  tokens[, N := 1]
+  
+  # Create specific-gram data.tables
+  for(gram in seq(length(ns), 1)){
+    # Sum group instances
+    tokens <- tokens[, list(N = sum(N)),
+                       by=eval(paste(ns[seq(gram, 1)], collapse = ","))]
+    
+    # Set keys for fast searching
+    setkeyv(tokens, cols = ns[seq(gram, 1)])
+    
+    # Assign the object to the specified environment
+    assign(x = paste0("n", gram, "_", tag),
+           value = tokens[complete.cases(tokens)],
+           pos = where)
+  }
+}
--- scripts/tasks/task2_02_build_ngrams.R
+++ scripts/tasks/task2_02_build_ngrams.R
@@ -6,13 +6,17 @@
 library(package = dplyr) # For lag.default
 library(package = data.table) # for operation
 
-# Clear the working environment
-rm(list = ls()[!grepl("token", ls())])
-
 # Load tokens
 load(file = "./data/pre-proc/01_token_news.Rdata")
 load(file = "./data/pre-proc/01_token_blogs.Rdata")
 load(file = "./data/pre-proc/01_token_twitter.Rdata")
+
+# Clear the working environment
+rm(list = ls()[!grepl("token", ls())])
+
+# Source the ngram function
+source(file = "./R/ngrams.R")
+
 
 
 
@@ -20,79 +24,28 @@ load(file = "./data/pre-proc/01_token_twitter.Rdata")
 # 2-, 3- and 4-gram counts
 #------------------------------------------------------------------------------*
 
+# Number of parameters
+param <- 4
 
 # News
-begin <- Sys.time()
-
-n_grams_news <- data.table(n0 = unlist(lapply(token_news, c, NA, NA, NA)))
-n_grams_news <- n_grams_news[, n1 := lag(n0, n = 1)]
-n_grams_news <- n_grams_news[, n2 := lag(n1, n = 1)]
-n_grams_news <- n_grams_news[, n3 := lag(n2, n = 1)][!is.na(n0)]
-
-n4_news <- n_grams_news[, .N, by="n3,n2,n1,n0"]
-n3_news <- n4_news[, list(N = sum(N)), by="n2,n1,n0"]
-n2_news <- n3_news[, list(N = sum(N)), by="n1,n0"]
-n1_news <- n2_news[, list(N = sum(N)), by="n0"]
-
-n4_news <- n4_news[complete.cases(n4_news)]
-n3_news <- n3_news[complete.cases(n3_news)]
-n2_news <- n2_news[complete.cases(n2_news)]
-
-setkey(n4_news, n3, n2, n1, n0)
-setkey(n3_news, n2, n1, n0)
-setkey(n2_news, n1, n0)
-setkey(n1_news, n0)
-
-end <- Sys.time()
-
-# Total elapsed time
-cat("News time: ")
-end-begin
-
+microbenchmark::microbenchmark(
+  times = 1,
+  ngrams(tokens = token_news, maxn = param, tag = "news")
+)
 
 
 # Blogs
-begin <- Sys.time()
-
-n_grams_blogs <- data.table(n0 = unlist(lapply(token_blogs, c, NA, NA)))
-n_grams_blogs <- n_grams_blogs[, n1 := lag(n0, n = 1)]
-n_grams_blogs <- n_grams_blogs[, n2 := lag(n1, n = 1)][!is.na(n0)]
-
-n3_blogs <- n_grams_blogs[, .N, by="n2,n1,n0"]
-n2_blogs <- n3_blogs[, list(N = sum(N)), by="n1,n0"]
-n1_blogs <- n2_blogs[, list(N = sum(N)), by="n0"]
-
-setkey(n3_blogs, n2, n1, n0)
-setkey(n2_blogs, n1, n0)
-setkey(n1_blogs, n0)
-
-end <- Sys.time()
-
-# Total elapsed time
-cat("Blogs time: ")
-end-begin
+microbenchmark::microbenchmark(
+  times = 1,
+  ngrams(tokens = token_blogs, maxn = param, tag = "blogs")
+)
 
 
 # Twitter
-begin <- Sys.time()
-
-n_grams_twitter <- data.table(n0 = unlist(lapply(token_twitter, c, NA, NA)))
-n_grams_twitter <- n_grams_twitter[, n1 := lag(n0, n = 1)]
-n_grams_twitter <- n_grams_twitter[, n2 := lag(n1, n = 1)][!is.na(n0)]
-
-n3_twitter <- n_grams_twitter[, .N, by="n2,n1,n0"]
-n2_twitter <- n3_twitter[, list(N = sum(N)), by="n1,n0"]
-n1_twitter <- n2_twitter[, list(N = sum(N)), by="n0"]
-
-setkey(n3_twitter, n2, n1, n0)
-setkey(n2_twitter, n1, n0)
-setkey(n1_twitter, n0)
-
-end <- Sys.time()
-
-# Total elapsed time
-cat("Twitter time: ")
-end-begin
+microbenchmark::microbenchmark(
+  times = 1,
+  ngrams(tokens = token_twitter, maxn = param, tag = "twitter")
+)
 
 
 # Save
