a841adbd2535cfa2f92b10f9c26e76f75de92c64
odeleongt [nb] <odeleongt@gmail.com>
2015-03-30 18:06:39 -0600

    Process the n-grams

--- scripts/tasks/task2_02_build_ngrams.R
+++ scripts/tasks/task2_02_build_ngrams.R
@@ -51,3 +51,27 @@ microbenchmark::microbenchmark(
 # Save
 save(list = grep(paste0("n[1-", param, "]"), ls(), value = TRUE),
      file = "./data/processed/02_ngrams.RData")
+
+
+
+
+# Get interesting words
+load(file = "./data/thesaurus/common_words.RData")
+
+
+# Only use referenced words
+words <- words[!is.na(type)]
+
+
+# Save some counts for description
+invisible(
+  sapply(
+    X =  grep(paste0("n[1-", param, "]"), ls(), value = TRUE),
+    FUN = function(data){
+      temp <- get(data)[order(-N)][1:20]
+      saveRDS(temp,
+           file = paste0("./data/processed/top-grams/", data, ".rds"))
+      temp <- get(data)[n0 %in% uwords][order(-N)][1:20]
+      saveRDS(temp,
+           file = paste0("./data/processed/top-grams/thes_", data, ".rds"))
+    }))
